87ce591f003f59a00992af95dd86742d3c3711c5
Burvan <burvani@mail.ru>
2025-02-28 14:06:50 +0300

    add domain logic

--- domain/lib/repositories/photo_repository.dart
+++ domain/lib/repositories/photo_repository.dart
@@ -2,4 +2,6 @@ import 'package:domain/domain.dart';
 
 abstract interface class PhotoRepository {
   Future<List<Photo>> getTrendingPhotos({int page = 0});
+  Future<void> saveToGallery(String photoUrl);
+  Future<void> sharePhoto(String photoUrl);
 }
