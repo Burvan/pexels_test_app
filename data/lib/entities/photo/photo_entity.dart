814379979887b9b5629782a0035aa5ef7f7d3fa7
Burvan <burvani@mail.ru>
2025-03-04 17:31:34 +0300

    generate hive adapters for entities

--- data/lib/entities/photo/photo_entity.dart
+++ data/lib/entities/photo/photo_entity.dart
@@ -1,20 +1,43 @@
+import 'package:core/core.dart';
 import 'package:data/entities/photo/src_entity.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'photo_entity.g.dart';
 
+@HiveType(typeId: 2)
 @JsonSerializable(createToJson: false)
 class PhotoEntity {
+  @HiveField(0)
   final int id;
+
+  @HiveField(1)
   final int? width;
+
+  @HiveField(2)
   final int? height;
+
+  @HiveField(3)
   final String? url;
+
+  @HiveField(4)
   final String? photographer;
+
+  @HiveField(5)
   final String? photographerUrl;
+
+  @HiveField(6)
   final int? photographerId;
+
+  @HiveField(7)
   final String? avgColor;
+
+  @HiveField(8)
   final String? alt;
+
+  @HiveField(9)
   final bool liked;
+
+  @HiveField(10)
   final SrcEntity src;
 
   PhotoEntity({
