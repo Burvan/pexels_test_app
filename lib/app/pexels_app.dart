fda34d3361064ab90bfbf2c0cfa2b0d00921e3d3
Burvan <burvani@mail.ru>
2025-03-08 15:08:09 +0300

    create ui for settings screen

--- lib/app/pexels_app.dart
+++ lib/app/pexels_app.dart
@@ -1,16 +1,30 @@
 import 'package:core/core.dart';
+import 'package:core_ui/core_ui.dart';
+import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
-import 'package:main_page/main_page.dart';
 import 'package:navigation/navigation.dart';
+import 'package:settings_page/settings_page.dart';
 
 class PexelsApp extends StatelessWidget {
   const PexelsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: appLocator.get<AppRouter>().delegate(),
-      routeInformationParser: appLocator.get<AppRouter>().defaultRouteParser(),
+    return BlocProvider(
+      create: (_) => SettingsBloc(
+        getThemeUseCase: appLocator.get<GetThemeUseCase>(),
+        setThemeUseCase: appLocator.get<SetThemeUseCase>(),
+      ),
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (BuildContext context, SettingsState state) {
+          return MaterialApp.router(
+            routerDelegate: appLocator.get<AppRouter>().delegate(),
+            routeInformationParser:
+                appLocator.get<AppRouter>().defaultRouteParser(),
+            theme: state.isDark ? AppTheme.darkTheme : AppTheme.lightTheme,
+          );
+        },
+      ),
     );
   }
 }
